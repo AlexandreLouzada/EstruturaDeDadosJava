8479660cc99763cf921cbc17a3c39299f78cd7cc
AlexandreLouzada <49560198+AlexandreLouzada@users.noreply.github.com>
2025-02-24 20:46:25 -0300

    Update TestaArray.java

--- Aula03b/TestaArray.java
+++ Aula03b/TestaArray.java
@@ -1,1 +1,32 @@
+public class TesteListaString {
+    public static void main(String[] args) {
+        ListaArrayString lista = new ListaArrayString(5);
+
+        // Adicionando nomes à lista
+        lista.adicionar("Ana");
+        lista.adicionar("Bruno");
+        lista.adicionar("Carlos");
+        System.out.println("Após adicionar nomes:");
+        lista.imprimirLista();
+
+        // Removendo um nome da posição 1
+        lista.remover(1);
+        System.out.println("Após remover o nome da posição 1:");
+        lista.imprimirLista();
+
+        // Buscando um nome na lista
+        String nomeBuscado = "Carlos";
+        int posicao = lista.buscar(nomeBuscado);
+        if (posicao != -1) {
+            System.out.println("O nome '" + nomeBuscado + "' foi encontrado na posição " + posicao);
+        } else {
+            System.out.println("O nome '" + nomeBuscado + "' não foi encontrado na lista.");
+        }
+
+        // Adicionando um novo nome
+        lista.adicionar("Daniela");
+        System.out.println("Após adicionar o nome 'Daniela':");
+        lista.imprimirLista();
+    }
+}
 
